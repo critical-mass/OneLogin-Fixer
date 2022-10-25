2f2727a82936e5420757574d24a3e3c3cb0d4184
Daniel Thompson <thompson9451@gmail.com>
2022-10-25 14:44:09 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,21 @@
-from curses import keyname
 import json
 import requests
 
-OL_client_id =  ###input("Please enter your OneLogin client ID: ")
-OL_client_secret =  ###input("Please enter your OneLogin client secret: ")
+OL_client_id = "" ###input("Please enter your OneLogin client ID: ")
+OL_client_secret = "" ###input("Please enter your OneLogin client secret: ")
 OL_url = ""
 Okta_API_Key = input("Please enter in your Okta API Key: ")
 
 
-class Oktauser():
-    def __init__(self, email, firstName, lastName, department, title, phoneNumber):
-        self.email = email
-        self.firstName = firstName
-        self.lastName = lastName
-        self.department = department 
-        self.title = title
-        self.phoneNumer = phoneNumber
-
 
 class oneLogin():
-    def __init__(self, OL_url, OL_client_id, OL_client_secret):
+    def __init__(self, OL_url, OL_client_id, OL_client_secret, OL_access_token):
         self.OL_url = OL_url
         self.OL_client_id = OL_client_id
         self.OL_client_secret = OL_client_secret
+        self.OL_access_token = OL_access_token
 
-    def OL_key_gen(OL_url, OL_client_id, OL_client_secret):
+    def key_gen(OL_url, OL_client_id, OL_client_secret):
         OL_auth_url = "https://" + OL_url + ".onelogin.com/auth/oauth2/v2/token"
         r = requests.post(OL_auth_url, auth=(OL_client_id, OL_client_secret), data={
             "grant_type": "client_credentials"
@@ -33,16 +24,17 @@ class oneLogin():
         OL_access_token=(response['access_token'])
         return OL_access_token
 
-    def listUsers(OL_url, key):
+    def listUsers(OL_url, OL_access_token):
         url=("https://" + OL_url + ".onelogin.com/api/2/users")
         payload={}
         headers = {
-        'Authorization': 'Bearer {key}'
-        }
-        r = requests.get(url, headers=headers, data=payload)
-        print(r.text)
-        return r
+        'Authorization': 'Bearer %s' % OL_access_token
+            }
+        response = requests.get(url, headers=headers, data=payload)
+        return response
 
-key = oneLogin.OL_key_gen(OL_url, OL_client_id, OL_client_secret)
-users = oneLogin.listUsers(OL_url, key)
-print(users)
+#gen your OL token
+OL_access_token = oneLogin.key_gen(OL_url, OL_client_id, OL_client_secret)
+users = oneLogin.listUsers(OL_url, OL_access_token)
+for x in users:
+    print(x)
